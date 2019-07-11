9d27552857c7abda8cbe0e056400f1dcf77ae222
ayotycoon <38573905+ayotycoon@users.noreply.github.com>
2019-07-11 22:18:08 +0100

    fifth commit

--- routes/api/api.js
+++ routes/api/api.js
@@ -66,7 +66,6 @@ router.post('/setMyProfileData', checkAuth, (req, res) => {
         .then(data => {
 
             if (data) {
-                data.img = req.body.img; 
                 data.role = req.body.role;
                 data.address = req.body.address;
                 data.name = req.body.name;
